b2b4ff213666699308e0ed936e641a1942370804
Karen Lee <leekaren888@gmail.com>
2018-09-07 00:55:46 -0700

    Created Fragments

--- app/src/main/java/me/kalee/aevus/LogInActivity.java
+++ app/src/main/java/me/kalee/aevus/LogInActivity.java
@@ -1,23 +1,64 @@
 package me.kalee.aevus;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
-public class LogInActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LogInActivity extends AppCompatActivity implements LogInFragment.OnFragmentInteractionListener,
+                                                                SignUpFragment.OnFragmentInteractionListener,
+                                                                GetLogInFragment.OnFragmentInteractionListener {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_log_in);
+
+        LogInFragment fragment = new LogInFragment();
+
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.add(R.id.container, fragment, "Log In");
+        ft.commit();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 
     public void goSignUp(View V) {
-        Intent goSignUp = new Intent(LogInActivity.this, SignUpActivity.class);
-        goSignUp.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
-        startActivity(goSignUp);
+        SignUpFragment fragment = new SignUpFragment();
+        transaction(fragment);
     }
 
+    public void goLogIn(View V) {
+        LogInFragment fragment = new LogInFragment();
+        transaction(fragment);
+    }
+
+    public void getLogIn(View V) {
+        GetLogInFragment fragment = new GetLogInFragment();
+        transaction(fragment);
+    }
+
+    private void transaction(Fragment fragment) {
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.container, fragment, "Sign Up");
+        ft.addToBackStack(null);
+        ft.commit();
+    }
 }
