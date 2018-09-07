e923962fd8f442964a0679729bea15814438a623
Karen Lee <leekaren888@gmail.com>
2018-09-06 18:19:08 -0700

    Prep Sign Up

--- app/src/main/java/me/kalee/aevus/SignUpActivity.java
+++ app/src/main/java/me/kalee/aevus/SignUpActivity.java
@@ -3,21 +3,20 @@ package me.kalee.aevus;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
-public class LogInActivity extends AppCompatActivity {
+public class SignUpActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
-        setContentView(R.layout.activity_log_in);
+        setContentView(R.layout.activity_sign_up);
     }
 
-    public void goSignUp(View V) {
-        Intent goSignUp = new Intent(LogInActivity.this, SignUpActivity.class);
-        goSignUp.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
-        startActivity(goSignUp);
+    public void goLogIn(View V) {
+        Intent goLogIn = new Intent(SignUpActivity.this, LogInActivity.class);
+        startActivity(goLogIn);
     }
-
 }
